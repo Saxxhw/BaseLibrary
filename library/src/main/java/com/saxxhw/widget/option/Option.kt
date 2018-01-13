0d68a1cab42cc7958d4c224af110c8596e297ebb
Saxxhw <wang921018>
2018-01-13 10:39:17 +0800

    1.0.4 1、修复Option未序列号导致程序闪退的bug

--- library/src/main/java/com/saxxhw/widget/option/Option.kt
+++ library/src/main/java/com/saxxhw/widget/option/Option.kt
@@ -1,10 +1,58 @@
 package com.saxxhw.widget.option
 
+import android.os.Parcel
+import android.os.Parcelable
+
 /**
  * Created by Saxxhw on 2018/1/12.
  * 邮箱：Saxxhw@126.com
  * 功能：
  */
-data class Option(val code: String, val stepCode: String, val name: String, val image: Image, var isChecked: Boolean)
+data class Option(val code: String, val stepCode: String, val name: String, val image: Image, var isChecked: Boolean) : Parcelable {
 
-data class Image(val url: String)
+    constructor(source: Parcel) : this(
+            source.readString(),
+            source.readString(),
+            source.readString(),
+            source.readParcelable<Image>(Image::class.java.classLoader), 1 == source.readInt()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeString(code)
+        writeString(stepCode)
+        writeString(name)
+        writeParcelable(image, 0)
+        writeInt((if (isChecked) 1 else 0))
+    }
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<Option> = object : Parcelable.Creator<Option> {
+            override fun createFromParcel(source: Parcel): Option = Option(source)
+            override fun newArray(size: Int): Array<Option?> = arrayOfNulls(size)
+        }
+    }
+}
+
+data class Image(val url: String) : Parcelable {
+
+    constructor(source: Parcel) : this(
+            source.readString()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeString(url)
+    }
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<Image> = object : Parcelable.Creator<Image> {
+            override fun createFromParcel(source: Parcel): Image = Image(source)
+            override fun newArray(size: Int): Array<Image?> = arrayOfNulls(size)
+        }
+    }
+}
