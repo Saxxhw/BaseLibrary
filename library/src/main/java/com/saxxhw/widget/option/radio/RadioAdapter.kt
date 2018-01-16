faf2830ead2026c2dd0e827d3069458196a4a4a5
Saxxhw <wang921018>
2018-01-16 16:19:39 +0800

    1.0.7 1、单选、多选控件图片不显示问题修复

--- library/src/main/java/com/saxxhw/widget/option/radio/RadioAdapter.kt
+++ library/src/main/java/com/saxxhw/widget/option/radio/RadioAdapter.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.saxxhw.R
+import com.saxxhw.glide.GlideApp
 import com.saxxhw.widget.option.Option
 
 /**
@@ -14,6 +15,7 @@ import com.saxxhw.widget.option.Option
 class RadioAdapter : BaseQuickAdapter<Option, BaseViewHolder>(R.layout.widget_radio_view, null) {
 
     override fun convert(helper: BaseViewHolder, item: Option) {
+        GlideApp.with(mContext).load(item.image.url).into(helper.getView(R.id.image))
         helper.setBackgroundColor(R.id.layout, if (item.isChecked) Color.parseColor("#E5F3FE") else Color.parseColor("#FFFFFF"))
         helper.setTextColor(R.id.title, if (item.isChecked) Color.parseColor("#008EFF") else Color.parseColor("#757575"))
         helper.setText(R.id.title, "${helper.layoutPosition + 1}、${item.name}")
